a1cff7c9e7efa3393e39cd132672434d2270d5df
Philip <philipsskog@gmail.com>
2023-02-16 14:56:45 +0100

    add squareSum kata

--- src/kataSquareSum/main.java
+++ src/kataSquareSum/main.java
@@ -1,2 +1,22 @@
-package kataSquareSum;public class main {
+package kataSquareSum;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class main{
+    static int[] numberArray = {1,2,3};
+
+    public static void main(String[] args) {
+        System.out.println(squareSum(numberArray));
+    }
+
+    public static int squareSum(int[] n){
+        int sum = 0;
+        for (double banan:n) {
+            banan = Math.pow(banan, 2);
+            sum += banan;
+        }
+        return sum;
+    }
+
 }
